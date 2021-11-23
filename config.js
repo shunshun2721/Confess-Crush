08f0d8a47b98924641e52e7bd91a199036b8eee5
shunshun2721 <94915850+shunshun2721@users.noreply.github.com>
2021-11-23 22:05:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 const CONFIG = {
     titleWeb: "Xin lỗi bé Linh",
     introTitle: 'bé Linh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Tui iu bé Linh nhất mà :<`,
     btnIntro: ':<',
     title: 'Tui xin lỗiiiii',
     desc: 'Đừng giận tui nữa mà :<',
     btnYes: 'Hết giận',
     btnNo: 'Hong',
     question: 'Hết giận tui nha :<',
-    btnReply: 'tui xin lỗi :((((((',
+    btnReply: 'có đi mừ :((((((',
     reply: 'Hong',
     mess: 'Đi mừ :<<',
     messDesc: '',
